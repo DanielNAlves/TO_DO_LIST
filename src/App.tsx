775f52ecc3b6475b3f1c2cabc5d21065c248c6d2
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-09-26 20:51:00 -0300

    style: Aplica formatacao padrao e ajusta estilos globais
    - Aplica a formatacao padrao (indentacao e espacamento) ao App.tsx e outros arquivos do projeto.
    - Pequenos ajustes em index.css para estilos iniciais.
    - Configura o linter/formatter (eslint.config.js).

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Text from "./components/text";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import CheckIcon from "./assets/icons/check.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
+import Icon from "./components/icon";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-1">
+        <Text variant={"body-sm-bold"} className="text-pink-base">
+          Olá mundo!
+        </Text>
+        <Text className="text-green-base">Olá mundo!</Text>
+        <Text variant={"body-md-bold"}>Olá mundo!</Text>
+        <Text>Levar o dog para passear!</Text>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-green-base" />
+        <Icon svg={CheckIcon} className="fill-green-base" />
+        <Icon svg={PencilIcon} className="fill-green-base" />
+        <Icon svg={PlusIcon} className="fill-green-base" />
+        <Icon svg={SpinnerIcon} animate />
+        <Icon svg={XIcon} className="fill-green-base" />
+      </div>
+    </div>
+  );
+}
