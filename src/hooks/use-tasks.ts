dffabbacc0210c12aeae5aaac5365cb0dfa8938a
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-10-06 11:58:59 -0300

    refactor(tasks): Ajusta hook de listagem para nova arquitetura de estado

---
+++ src/hooks/use-tasks.ts
@@ -0,0 +1,33 @@
+import useLocalStorage from "use-local-storage";
+import { TASKS_KEY, TaskState, type Task } from "../models/task";
+import React from "react";
+import { delay } from "../helpers/utils";
+
+export default function useTasks() {
+  const [tasksData] = useLocalStorage<Task[]>(TASKS_KEY, []);
+  const [tasks, setTasks] = React.useState<Task[]>([]);
+  const [isLoadingTasks, setIsLoadingTasks] = React.useState<boolean>(true);
+
+  async function fetchTasks() {
+    if (isLoadingTasks) {
+      console.time("Carregando tarefas...");
+      await delay(2000);
+      setIsLoadingTasks(false);
+    }
+    setTasks(tasksData);
+    console.timeEnd("Carregando tarefas...");
+  }
+
+  React.useEffect(() => {
+    fetchTasks();
+  }, [tasksData]);
+
+  return {
+    tasks,
+    createdTasksCount: tasks.filter(
+      (task: Task) => task.state === TaskState.Created
+    ).length,
+    concludedTasksCount: tasks.filter((task: Task) => task.concluded).length,
+    isLoadingTasks,
+  };
+}
