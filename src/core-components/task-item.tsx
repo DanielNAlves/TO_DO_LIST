ad5b7c6532fff04f2107f20aa5168104296e848f
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-10-06 11:57:25 -0300

    feat(context): Implementa TaskProvider para gerenciar estados de loading por ID

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -8,45 +8,129 @@ import CheckIcon from "../assets/icons/check.svg?react";
 import PencilIcon from "../assets/icons/pencil.svg?react";
 import XIcon from "../assets/icons/x.svg?react";
 import InputText from "../components/input";
+import { TaskState, type Task } from "../models/task";
+import { cx } from "class-variance-authority";
+import Skeleton from "../components/skeleton";
+import useTask from "../hooks/use-task";
 
-export default function TaskItem() {
-  const [isEditing, setIsEditing] = React.useState(false);
+interface TaskItemProps {
+  task: Task;
+  updateTask: (id: string, payload: { title: string }) => void;
+  updateTaskStatus: (id: string, concluded: boolean) => void;
+  deleteTask: (id: string) => void;
+  loading?: boolean;
+  isUpdatingTask?: boolean;
+  isDeletingTask?: boolean;
+}
+
+export default function TaskItem({
+  task,
+  loading,
+  updateTask: updateTaskProp,
+  updateTaskStatus,
+  deleteTask,
+}: TaskItemProps) {
+  const [isEditing, setIsEditing] = React.useState(
+    task?.state === TaskState.Creating
+  );
+
+  const [taskTitle, setTaskTitle] = React.useState(task.title || "");
+  const { isUpdatingTask, isDeletingTask } = useTask();
 
   function handleEditTask() {
     setIsEditing(true);
   }
 
   function handleExitEditTask() {
+    if (task.state === TaskState.Creating) {
+      deleteTask(task.id);
+    }
     setIsEditing(false);
   }
 
+  function handleChangeTaskTitle(e: React.ChangeEvent<HTMLInputElement>) {
+    setTaskTitle(e.target.value || "");
+  }
+
+  async function handleSaveTask(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    await updateTaskProp(task.id, { title: taskTitle });
+
+    setIsEditing(false);
+  }
+
+  function handleChangeTaskStatus(e: React.ChangeEvent<HTMLInputElement>) {
+    const checked = e.target.checked;
+
+    updateTaskStatus(task.id, checked);
+  }
+
+  async function handleDeleteTask() {
+    await deleteTask(task.id);
+  }
+
   return (
-    <Card size="md" className="flex items-center gap-4">
+    <Card size="md">
       {!isEditing ? (
-        <>
-          <InputCheckbox />
-          <Text className="flex-1">🛒Fazer compras da Semana</Text>
+        <div className="flex items-center gap-4">
+          <InputCheckbox
+            checked={task?.concluded}
+            onChange={handleChangeTaskStatus}
+            loading={loading}
+          />
+          {!loading ? (
+            <Text
+              className={cx("flex-1", {
+                "line-through": task?.concluded,
+              })}
+            >
+              {task?.title}
+            </Text>
+          ) : (
+            <Skeleton className="flex-1 h-6" />
+          )}
           <div className="flex gap-1">
-            <ButtonIcon icon={TrashIcon} variant="tertiary" />
             <ButtonIcon
+              icon={TrashIcon}
+              variant="tertiary"
+              onClick={handleDeleteTask}
+              loading={loading}
+              handling={isDeletingTask}
+            />
+            <ButtonIcon
+              type="button"
               icon={PencilIcon}
               variant="tertiary"
               onClick={handleEditTask}
+              loading={loading}
             />
           </div>
-        </>
+        </div>
       ) : (
-        <>
-          <InputText className="flex-1" />
+        <form onSubmit={handleSaveTask} className="flex items-center gap-4">
+          <InputText
+            className="flex-1"
+            value={taskTitle}
+            onChange={handleChangeTaskTitle}
+            required
+            autoFocus
+          />
           <div className="flex gap-1">
             <ButtonIcon
+              type="button"
               icon={XIcon}
               variant="secondary"
               onClick={handleExitEditTask}
             />
-            <ButtonIcon icon={CheckIcon} variant="primary" />
+            <ButtonIcon
+              type="submit"
+              icon={CheckIcon}
+              variant="primary"
+              handling={isUpdatingTask}
+            />
           </div>
-        </>
+        </form>
       )}
     </Card>
   );
