65d2fc5eca5ca140df127bf9ceae04f5dbfb8714
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-10-06 11:57:44 -0300

    feat(context): Implementa TaskProvider para gerenciar estados de loading por ID

--- src/core-components/tasks-list.tsx
+++ src/core-components/tasks-list.tsx
@@ -1,20 +1,72 @@
 import Button from "../components/button";
 import PlusIcon from "../assets/icons/plus.svg?react";
 import TaskItem from "./task-item";
+import useTasks from "../hooks/use-tasks";
+import useTask from "../hooks/use-task";
+import { TaskState, type Task } from "../models/task";
 
-export default function TaskList() {
+export default function TasksList() {
+  const { tasks, isLoadingTasks } = useTasks();
+  const { prepareTask, updateTask, updateTaskStatus, deleteTask } = useTask();
+
+  function handleNewTask() {
+    prepareTask();
+  }
+
   return (
     <>
       <section>
-        <Button icon={PlusIcon} className="w-full">
+        <Button
+          icon={PlusIcon}
+          className="w-full"
+          onClick={handleNewTask}
+          handling={isLoadingTasks}
+          disabled={
+            tasks.some((task) => task.state === TaskState.Creating) ||
+            isLoadingTasks
+          }
+        >
           Nova tarefa
         </Button>
       </section>
+
       <section className="space-y-2">
-        <TaskItem />
-        <TaskItem />
-        <TaskItem />
-        <TaskItem />
+        {!isLoadingTasks &&
+          tasks.map((task) => (
+            <TaskItem
+              key={task.id}
+              task={task}
+              updateTask={updateTask}
+              updateTaskStatus={updateTaskStatus}
+              deleteTask={deleteTask}
+            />
+          ))}
+
+        {isLoadingTasks && (
+          <>
+            <TaskItem
+              task={{} as Task}
+              updateTask={() => {}}
+              updateTaskStatus={() => {}}
+              deleteTask={() => {}}
+              loading
+            />
+            <TaskItem
+              task={{} as Task}
+              updateTask={() => {}}
+              updateTaskStatus={() => {}}
+              deleteTask={() => {}}
+              loading
+            />
+            <TaskItem
+              task={{} as Task}
+              updateTask={() => {}}
+              updateTaskStatus={() => {}}
+              deleteTask={() => {}}
+              loading
+            />
+          </>
+        )}
       </section>
     </>
   );
