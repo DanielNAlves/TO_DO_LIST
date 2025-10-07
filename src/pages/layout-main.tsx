2a08b35adc05e3067997993ca5dad89fa93603ec
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-10-06 21:19:10 -0300

    refactor realizado organização do código.

--- src/pages/layout-main.tsx
+++ src/pages/layout-main.tsx
@@ -1,7 +1,8 @@
 import { Outlet } from "react-router";
+
 import Header from "../core-components/header";
-import MainContent from "../core-components/main-content";
 import Footer from "../core-components/footer";
+import MainContent from "../core-components/main-content";
 
 export default function LayoutMain() {
   return (
